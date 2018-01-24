94973fb06820e16a1198a262f7e14072315a3dd4
Team 4 <gint271@gmail.com>
2018-01-24 13:45:24 -0800

    More of lab 15 (not working yet)

--- fetch_api/src/fetch_api/base.py
+++ fetch_api/src/fetch_api/base.py
@@ -4,6 +4,8 @@ import rospy
 
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
+import tf.transformations as tft
+import numpy as np
 
 import copy
 import math
@@ -67,7 +69,6 @@ class Base(object):
     def _odom_callback(self, msg):
         """
         :param msg: nav_msgs/Odometry Message
-        :return:
         """
         self.has_received_odom_msg = True
         self.last_position = msg.pose.pose  # contains 'position' and 'orientation'
@@ -89,7 +90,7 @@ class Base(object):
         while not self.has_received_odom_msg:
             rospy.sleep(0.5)
 
-        # TODO: record start position, use Python's copy.deepcopy
+        # record start position, use Python's copy.deepcopy
         start_pos = copy.deepcopy(self.last_position.position)
         rate = rospy.Rate(10)
         # CONDITION should check if the robot has traveled the desired distance
@@ -103,9 +104,59 @@ class Base(object):
             traveled_distance = math.sqrt(math.pow((current_position.x - start_pos.x), 2) +
                                           math.pow((current_position.y - start_pos.y), 2) +
                                           math.pow((current_position.z - start_pos.z), 2))
-            # TODO: you will probably need to do some math in this loop to check the CONDITION
             direction = -1 if distance < 0 else 1
             self.move(direction * speed, 0)
             if traveled_distance >= math.fabs(distance):
                 break
             rate.sleep()
+
+    def turn(self, angular_distance, speed=0.5):
+        """Rotates the robot a certain angle.
+
+        Args:
+            angular_distance: The angle, in radians, to rotate. A positive
+                value rotates counter-clockwise.
+            speed: The angular speed to rotate, in radians/second.
+        """
+        # rospy.sleep until the base has received at least one message on /odom
+        while not self.has_received_odom_msg:
+            rospy.sleep(0.5)
+
+        # record start position
+        start_orientation = copy.deepcopy(
+            self.last_position.orientation)  # *shudders* it's a Quaternion, not sure how to use
+        start_yaw = self.quaternion_to_yaw(start_orientation)
+        # TODO: What will you do if angular_distance is greater than 2*pi or less than -2*pi?
+
+        # do we even care if angular_distance is < or > 2pi? shouldn't we just spin around that many times?
+        polarity = -1 if angular_distance < 0 else 1
+        finish_angle = angular_distance % (2 * math.pi) * polarity
+        current_orientation = copy.deepcopy(self.last_position.orientation)
+        # Idea: get yaw (rotation abt z azis)
+        current_yaw = self.quaternion_to_yaw(current_orientation)
+        traveled_angle = math.fabs(current_yaw - start_yaw)
+
+        rate = rospy.Rate(10)
+        # TODO: CONDITION should check if the robot has rotated the desired amount
+        # TODO: Be sure to handle the case where the desired amount is negative!
+        while (not rospy.is_shutdown() and traveled_angle < math.fabs(finish_angle)):
+            # TODO: you will probably need to do some math in this loop to check the CONDITION
+            # need to calculate how much the angle has changed, need to deal with "wraparound" issue
+
+            direction = -1 if angular_distance < 0 else 1
+            self.move(0, direction * speed)
+            rate.sleep()
+
+    def quaternion_to_yaw(q):
+        """
+        Automates getting yaw for navigation. Only rotation parameter that matters to you for navigation is Θ, the rotation about the Z-axis.
+        See https://github.com/cse481wi18/cse481wi18/wiki/Lab-14%3A-Odometry-and-rotations#whats-my-yaw for mathematical explanation
+        Args:
+            q: type: geometry_msgs/Quaternion.msg. has paramams w x y z
+        :return: yaw in radians
+        """
+        rotation_matrix = tft.quaternion_matrix([q.x, q.y, q.z, q.w])
+        x = rotation_matrix[0, 0]
+        y = rotation_matrix[1, 0]
+        theta_rads = math.atan2(y, x)
+        return theta_rads
