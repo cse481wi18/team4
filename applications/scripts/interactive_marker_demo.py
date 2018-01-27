06449e2bc0ea38d32a31b5cbe73acc43bf8c4842
Team 4 <gint271@gmail.com>
2018-01-27 11:35:36 -0800

    setup for blog post 1

--- applications/scripts/interactive_marker_demo.py
+++ applications/scripts/interactive_marker_demo.py
@@ -3,6 +3,8 @@
 from interactive_markers.interactive_marker_server import InteractiveMarkerServer
 from visualization_msgs.msg import InteractiveMarker, InteractiveMarkerControl, InteractiveMarkerFeedback
 from visualization_msgs.msg import Marker
+import fetch_api
+import math
 import rospy
 
 
@@ -12,6 +14,7 @@ def wait_for_time():
     while rospy.Time().now().to_sec() == 0:
         pass
 
+base = fetch_api.Base()
 
 def handle_viz_input(input):
     if (input.event_type == InteractiveMarkerFeedback.BUTTON_CLICK):
@@ -19,6 +22,25 @@ def handle_viz_input(input):
     else:
         rospy.loginfo('Cannot handle this InteractiveMarker event')
 
+def go_forward(input):
+    if (input.event_type == InteractiveMarkerFeedback.BUTTON_CLICK):
+        rospy.loginfo("going forward")
+        base.go_forward(0.5)
+
+def go_backward(input):
+    if (input.event_type == InteractiveMarkerFeedback.BUTTON_CLICK):
+        rospy.loginfo("going backward")
+        base.go_forward(-0.5)
+
+def turn_port(input):
+    if (input.event_type == InteractiveMarkerFeedback.BUTTON_CLICK):
+        rospy.loginfo("turning port")
+        base.turn(45 * math.pi / 180)
+
+def turn_starboard(input):
+    if (input.event_type == InteractiveMarkerFeedback.BUTTON_CLICK):
+        rospy.loginfo("turning starboard")
+        base.turn(-45 * math.pi / 180)
 
 def main():
     rospy.init_node('nav_node')
@@ -27,33 +49,108 @@ def main():
     # Create server
     server = InteractiveMarkerServer("simple_marker")
     # Create interactive marker
-    int_marker = InteractiveMarker()
-    int_marker.header.frame_id = "base_link"
-    int_marker.name = "my_marker"
-    int_marker.description = "It's a box!"
-    int_marker.pose.position.x = 1
-    int_marker.pose.orientation.w = 1
+    fwd_marker = InteractiveMarker()
+    fwd_marker.header.frame_id = "base_link"
+    fwd_marker.name = "fwd_marker"
+    fwd_marker.description = "Go forward"
+    fwd_marker.pose.position.x = 1
+    fwd_marker.pose.orientation.w = 1
     # create teal cube marker for the interactive marker
-    box_marker = Marker()
-    box_marker.type = Marker.CUBE
-    box_marker.pose.orientation.w = 1
-    box_marker.scale.x = 0.45
-    box_marker.scale.y = 0.45
-    box_marker.scale.z = 0.45
-    box_marker.color.r = 0.0
-    box_marker.color.g = 0.5
-    box_marker.color.b = 0.5
-    box_marker.color.a = 1.0
+    fbox_marker = Marker()
+    fbox_marker.type = Marker.CUBE
+    fbox_marker.pose.orientation.w = 1
+    fbox_marker.scale.x = 0.45
+    fbox_marker.scale.y = 0.45
+    fbox_marker.scale.z = 0.45
+    fbox_marker.color.r = 0.5
+    fbox_marker.color.g = 0.5
+    fbox_marker.color.b = 0.0
+    fbox_marker.color.a = 1.0
+
+    bkw_marker = InteractiveMarker()
+    bkw_marker.header.frame_id = "base_link"
+    bkw_marker.name = "bkw_marker"
+    bkw_marker.description = "Go backward"
+    bkw_marker.pose.position.x = -1
+    bkw_marker.pose.orientation.w = 1
+    # create teal cube marker for the interactive marker
+    bbox_marker = Marker()
+    bbox_marker.type = Marker.CUBE
+    bbox_marker.pose.orientation.w = 1
+    bbox_marker.scale.x = 0.45
+    bbox_marker.scale.y = 0.45
+    bbox_marker.scale.z = 0.45
+    bbox_marker.color.r = 0.5
+    bbox_marker.color.g = 0.0
+    bbox_marker.color.b = 0.5
+    bbox_marker.color.a = 1.0
+
+    prt_marker = InteractiveMarker()
+    prt_marker.header.frame_id = "base_link"
+    prt_marker.name = "prt_marker"
+    prt_marker.description = "Turn port"
+    prt_marker.pose.position.y = -1
+    prt_marker.pose.orientation.w = 1
+    # create teal cube marker for the interactive marker
+    pbox_marker = Marker()
+    pbox_marker.type = Marker.CUBE
+    pbox_marker.pose.orientation.w = 1
+    pbox_marker.scale.x = 0.45
+    pbox_marker.scale.y = 0.45
+    pbox_marker.scale.z = 0.45
+    pbox_marker.color.r = 1.0
+    pbox_marker.color.g = 0.0
+    pbox_marker.color.b = 0.0
+    pbox_marker.color.a = 1.0
+
+    stb_marker = InteractiveMarker()
+    stb_marker.header.frame_id = "base_link"
+    stb_marker.name = "stb_marker"
+    stb_marker.description = "Turn starboard"
+    stb_marker.pose.position.y = 1
+    stb_marker.pose.orientation.w = 1
+    # create teal cube marker for the interactive marker
+    sbox_marker = Marker()
+    sbox_marker.type = Marker.CUBE
+    sbox_marker.pose.orientation.w = 1
+    sbox_marker.scale.x = 0.45
+    sbox_marker.scale.y = 0.45
+    sbox_marker.scale.z = 0.45
+    sbox_marker.color.r = 0.5
+    sbox_marker.color.g = 0.0
+    sbox_marker.color.b = 0.5
+    sbox_marker.color.a = 1.0
 
     # create an InteractiveMarkerControl, add the Marker to it, and
-    button_control = InteractiveMarkerControl()
+    fbutton_control = InteractiveMarkerControl()
     # add the control to the InteractiveMarker.
-    button_control.interaction_mode = InteractiveMarkerControl.BUTTON
-    button_control.always_visible = True
-    button_control.markers.append(box_marker)
-    int_marker.controls.append(button_control)
+    fbutton_control.interaction_mode = InteractiveMarkerControl.BUTTON
+    fbutton_control.always_visible = True
+    fbutton_control.markers.append(fbox_marker)
+    fwd_marker.controls.append(fbutton_control)
 
-    server.insert(int_marker, handle_viz_input)
+    bbutton_control = InteractiveMarkerControl()
+    bbutton_control.interaction_mode = InteractiveMarkerControl.BUTTON
+    bbutton_control.always_visible = True
+    bbutton_control.markers.append(bbox_marker)
+    bkw_marker.controls.append(bbutton_control)
+
+    pbutton_control = InteractiveMarkerControl()
+    pbutton_control.interaction_mode = InteractiveMarkerControl.BUTTON
+    pbutton_control.always_visible = True
+    pbutton_control.markers.append(pbox_marker)
+    prt_marker.controls.append(pbutton_control)
+
+    sbutton_control = InteractiveMarkerControl()
+    sbutton_control.interaction_mode = InteractiveMarkerControl.BUTTON
+    sbutton_control.always_visible = True
+    sbutton_control.markers.append(sbox_marker)
+    stb_marker.controls.append(sbutton_control)
+
+    server.insert(fwd_marker, go_forward)
+    server.insert(bkw_marker, go_backward)
+    server.insert(stb_marker, turn_starboard)
+    server.insert(prt_marker, turn_port)
     server.applyChanges()
     rospy.spin()
 
