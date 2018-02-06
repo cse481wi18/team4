911d0f05a33728901f450b08eb1b6efa7a9d3956
Team 4 <gint271@gmail.com>
2018-02-06 14:18:00 -0800

    Middle of lab 28

---
+++ perception/src/perception/mock_camera.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+import rosbag
+
+camera = perception.MockCamera()
+cloud = camera.read_cloud('~/data/tags.bag')
+
+class MockCamera(object): 
+    """A MockCamera reads saved point clouds.
+    """
+    def __init__(self):
+        pass
+
+    def read_cloud(self, path):
+        """Returns the sensor_msgs/PointCloud2 in the given bag file.
+    
+        Args:
+            path: string, the path to a bag file with a single
+            sensor_msgs/PointCloud2 in it.
+
+        Returns: A sensor_msgs/PointCloud2 message, or None if there were no
+            PointCloud2 messages in the bag file.
+        """
+        with rosbag.Bag(path) as bag:
+            for topic, msg, t in bag.read_messages(topics=['chatter', 'numbers']):
+                return msg
+        return None
