0a8fe63b5696a7b3d60b7840676865de012c5583
Team 4 <gint271@gmail.com>
2018-02-11 15:40:57 -0800

    build success, spin on point_cloud_demo

--- perception/src/crop.cpp
+++ perception/src/crop.cpp
@@ -1,5 +1,9 @@
+#include "../include/perception/crop.h"
+
+namespace perception {
 Cropper::Cropper() {}
 
 void Cropper::Callback(const sensor_msgs::PointCloud2& msg) {
   ROS_INFO("Got point cloud");
-}
+};
+} // namespace perception
