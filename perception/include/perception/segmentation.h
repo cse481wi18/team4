b08107771f73d06b5207cdeb788ed14290e6c105
Team 4 <gint271@gmail.com>
2018-02-11 17:26:46 -0800

    added stuff for getaxis, at lab31 - PointIndices

--- perception/include/perception/segmentation.h
+++ perception/include/perception/segmentation.h
@@ -3,6 +3,8 @@
 #include "pcl/point_types.h"
 #include "ros/ros.h"
 #include "sensor_msgs/PointCloud2.h"
+#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/Vector3.h"
 
 namespace perception {
 // Finds the largest horizontal surface in the given point cloud.
@@ -14,6 +16,17 @@ namespace perception {
 //    surface. Empty if no surface was found.
 void SegmentSurface(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,
                     pcl::PointIndices::Ptr indices);
+
+// Computes the axis-aligned bounding box of a point cloud.
+//
+// Args:
+//  cloud: The point cloud
+//  pose: The output pose. Because this is axis-aligned, the orientation is just
+//    the identity. The position refers to the center of the box.
+//  dimensions: The output dimensions, in meters.
+void GetAxisAlignedBoundingBox(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,
+                               geometry_msgs::Pose* pose,
+                               geometry_msgs::Vector3* dimensions);
 
 class Segmenter {
  public:
--- perception/src/segmentation.cpp
+++ perception/src/segmentation.cpp
@@ -7,11 +7,52 @@
 #include "ros/ros.h"
 #include "sensor_msgs/PointCloud2.h"
 
+#include "pcl/common/angles.h"
+#include "pcl/sample_consensus/method_types.h"
+#include "pcl/sample_consensus/model_types.h"
+#include "pcl/segmentation/sac_segmentation.h"
+
+#include "pcl/filters/extract_indices.h"
+
 typedef pcl::PointXYZRGB PointC;
 typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloudC;
 
 namespace perception {
+
 void SegmentSurface(PointCloudC::Ptr cloud, pcl::PointIndices::Ptr indices) {
+  pcl::PointIndices indices_internal;
+  pcl::SACSegmentation<PointC> seg;
+  seg.setOptimizeCoefficients(true);
+  // Search for a plane perpendicular to some axis (specified below).
+  seg.setModelType(pcl::SACMODEL_PERPENDICULAR_PLANE);
+  seg.setMethodType(pcl::SAC_RANSAC);
+  // Set the distance to the plane for a point to be an inlier.
+  seg.setDistanceThreshold(0.01);
+  seg.setInputCloud(cloud);
+
+  // Make sure that the plane is perpendicular to Z-axis, 10 degree tolerance.
+  Eigen::Vector3f axis;
+  axis << 0, 0, 1;
+  seg.setAxis(axis);
+  seg.setEpsAngle(pcl::deg2rad(10.0));
+
+  // coeff contains the coefficients of the plane:
+  // ax + by + cz + d = 0
+  pcl::ModelCoefficients coeff;
+  seg.segment(indices_internal, coeff);
+
+  *indices = indices_internal;
+
+  if (indices->indices.size() == 0) {
+    ROS_ERROR("Unable to find surface.");
+    return;
+  }
+}
+
+void GetAxisAlignedBoundingBox(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,
+                               geometry_msgs::Pose* pose,
+                               geometry_msgs::Vector3* dimensions) {
+   //TODO: implement this
 }
 
 Segmenter::Segmenter(const ros::Publisher& surface_points_pub)
@@ -21,6 +62,23 @@ void Segmenter::Callback(const sensor_msgs::PointCloud2& msg) {
   PointCloudC::Ptr cloud(new PointCloudC());
   pcl::fromROSMsg(msg, *cloud);
 
+  //PointCloudC::Ptr cloud(new PointCloudC);
+  pcl::PointIndices indices;
+  for (size_t i=0; i<indices.indices.size(); ++i) {
+    int index = indices.indices[i];
+    const PointC& pt = cloud->points[index];
+  }
+
+  // Given these data types:
+  PointCloudC::Ptr original_cloud(new PointCloudC);
+  //pcl::PointIndices indices;
+  PointCloudC::Ptr subset_cloud(new PointCloudC);
+
+  // Extract subset of original_cloud into subset_cloud:
+  pcl::ExtractIndices<PointC> extract;
+  extract.setInputCloud(original_cloud);
+  extract.setIndices(indices);
+  extract.filter(*subset_cloud);
   pcl::PointIndices::Ptr table_inliers(new pcl::PointIndices());
   SegmentSurface(cloud, table_inliers);
 }
