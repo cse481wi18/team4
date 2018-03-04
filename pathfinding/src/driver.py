7b61e98526c90f9191d445ac4189dbb4c40bfb33
mirandae <mirae@cs.washington.edu>
2018-03-03 23:34:08 -0800

    get current position

--- pathfinding/src/driver.py
+++ pathfinding/src/driver.py
@@ -2,9 +2,11 @@
 import actionlib
 import rospy
 import copy
-from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped, Pose
+import tf.transformations as tft
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped, Pose, Quaternion
 from std_msgs.msg import Header
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import math
 
 
 def printPose(stampedPose):
@@ -17,21 +19,29 @@ def printPose(stampedPose):
 class Driver(object):
     def __init__(self):
         # self._goto_publisher = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+        self._curr_pose_sub = rospy.Subscriber('/amcl_pose', PoseStamped, self.set_curr_map_pose)
         self._goto_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-        self.curr_goal_pose = None
+        self._curr_goal_pose = None
+        self._curr_map_pose = None
 
+    # msg
+    def set_curr_map_pose(self, curr_pose):
+        self._curr_map_pose = curr_pose.pose.pose.position
 
-    def go_to(self, pose_stamped):
-        self.curr_goal_pose = pose_stamped
-        print "going to ", pose_stamped
+    def go_to(self, pose):
+        self._curr_goal_pose = pose
+        print "going to ", pose
 
         goal_pose = PoseStamped()
         goal_pose.header.frame_id = "map"
-        goal_pose.pose = copy.deepcopy(pose_stamped)
+        goal_pose.pose = copy.deepcopy(pose)
+        quat_arr = tft.quaternion_from_euler(0, 0, math.atan2(pose.position.y - self._curr_map_pose.y, pose.position.x - self._curr_map_pose.x))
+        orientation = Quaternion(quat_arr[0], quat_arr[1], quat_arr[2], quat_arr[3])
+        goal_pose.pose.orientation = orientation
         move_goal = MoveBaseGoal()
         move_goal.target_pose = goal_pose
-        self._goto_client.send_goal_and_wait(move_goal) #blocks until move is successful
-        self.curr_goal_pose = None
+        self._goto_client.send_goal_and_wait(move_goal) # blocks until move is successful
+        self._curr_goal_pose = None
 
     # TODO blocking
     def return_to_default_position(self, pose_stamped):
