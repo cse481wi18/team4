2dc2d9baea2031fc9babee1ac005b8537052af5e
mirandae <mirae@cs.washington.edu>
2018-03-03 23:20:34 -0800

    cleaned up driver

--- pathfinding/src/driver.py
+++ pathfinding/src/driver.py
@@ -1,39 +1,37 @@
 
+import actionlib
 import rospy
 import copy
 from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped, Pose
 from std_msgs.msg import Header
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
-current_position = None
 
-# TODO milestone 1 implement class
+def printPose(stampedPose):
+    pose = stampedPose.pose.pose
+
+    print "Position: ", pose.position.x, pose.position.y, pose.position.z
+    print "Quaternion: ", pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w
 
 
 class Driver(object):
     def __init__(self):
-        self._goto_publisher = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+        # self._goto_publisher = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+        self._goto_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        self.curr_goal_pose = None
 
-
-    def printPose(self, stampedPose):
-        pose = stampedPose.pose.pose
-
-        print "Position: ", pose.position.x, pose.position.y, pose.position.z
-        print "Quaternion: ", pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w
-
-
-    def updateCurrentPose(self, msg):
-        global current_position
-        current_position = msg
 
     def go_to(self, pose_stamped):
-        # TODO blocking
-        newGoal = PoseStamped()
-        newGoal.header = copy.deepcopy(pose_stamped.header)
-        newGoal.header.frame_id = "map"
-        newGoal.pose = copy.deepcopy(pose_stamped.pose)
+        self.curr_goal_pose = pose_stamped
+        print "going to ", pose_stamped
 
-        self._goto_publisher.publish(newGoal)
-        # TODO wait and return
+        goal_pose = PoseStamped()
+        goal_pose.header.frame_id = "map"
+        goal_pose.pose = copy.deepcopy(pose_stamped)
+        move_goal = MoveBaseGoal()
+        move_goal.target_pose = goal_pose
+        self._goto_client.send_goal_and_wait(move_goal) #blocks until move is successful
+        self.curr_goal_pose = None
 
     # TODO blocking
     def return_to_default_position(self, pose_stamped):
