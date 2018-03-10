a5ccab895392341679a126ec13823075d9130772
Team 4 <gint271@gmail.com>
2018-03-09 18:40:12 -0800

    Finished perceptor.py, todo finish backend for perception

--- pathfinding/src/perceptor.py
+++ pathfinding/src/perceptor.py
@@ -1,33 +1,25 @@
 import rospy
-import copy
 from perception_msgs.msg import BallPositions
 from geometry_msgs.msg import Pose
-# TODO milestone 1 implement class
 
 
 class Perceptor:
     def __init__(self):
-        self._get_ball_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        self._get_ball_pub = rospy.Publisher('get_ball_locations', Pose, queue_size=5) # type of message does not matter
+
     # return None if no ball found
     # return some type of position (pose_stamped) (correspond to pick_up_ball arg)
     def get_closest_ball_location(self):
         print "[Perceptor: calling get_closest_ball_location]"
-        curr_poses = copy.deepcopy(self._curr_seen_ball_poses)
+        # send request for message
+        self._get_ball_pub.publish(Pose())
+        # block until we get a response (balls or no balls)
+        ball_positions_msg = rospy.wait_for_message("tennis_ball_position_topic", BallPositions)
+        curr_poses = ball_positions_msg.positions
         # currently returns first ball in arr, TODO get closest ball
         if len(curr_poses) is not 0:
             return curr_poses[0]
-        # fn should be non-blocking
         print "[Perceptor: no balls found]"
         return None
 
-    def __init__(self):
-        self._sub = rospy.Subscriber('tennis_ball_position_topic', BallPositions, self._set_curr_map_pose)
-        self._curr_seen_ball_poses = []  # type Pose
 
-
-    def _set_curr_map_pose(self, obj_feat_msg):
-        # print "calling _set_curr_map_pose"
-        # print "In perceptor, seeing: "
-        # for i in obj_feat_msg.positions:
-        #     print i
-        self._curr_seen_ball_poses = obj_feat_msg.positions
--- perception/src/tennis_ball_finder.cpp
+++ perception/src/tennis_ball_finder.cpp
@@ -20,19 +20,22 @@ void GetBallLocationCallback(const sensor_msgs::PointCloud2& msg) {
 
 int main(int argc, char** argv) {
 
-  ros::init(argc, argv, "point_cloud_demo");
+  ros::init(argc, argv, "tennis_ball_finder");
   ros::NodeHandle nh;
 
-  if (argc < 2) {
-    ROS_INFO("Usage: rosrun perception point_cloud_demo DATA_DIR");
-    ros::spinOnce();
-  }
+//  if (argc < 2) {
+//    ROS_INFO("Usage: rosrun perception point_cloud_demo DATA_DIR");
+//    ros::spinOnce();
+//  }
 
   ros::Publisher crop_pub =
       nh.advertise<sensor_msgs::PointCloud2>("cropped_cloud", 1, true);
   perception::Cropper cropper(crop_pub);
+
 //  ros::Subscriber sub =
 //      nh.subscribe("cloud_in", 1, &perception::Cropper::Callback, &cropper);
+
+
 
   ros::Publisher table_pub =
       nh.advertise<sensor_msgs::PointCloud2>("table_cloud", 1, true);
