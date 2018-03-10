6b282f3d9e2ce9a5053501ec526d11437c6cb257
Team 4 <gint271@gmail.com>
2018-03-09 19:05:58 -0800

    perception refactor done - needs to be tested

--- perception/src/tennis_ball_finder.cpp
+++ perception/src/tennis_ball_finder.cpp
@@ -5,37 +5,23 @@
 #include "perception/segmentation.h"
 #include "visualization_msgs/Marker.h"
 
+
 #include "perception/object_recognizer.h" // lab34
 #include "perception_msgs/BallPositions.h"
+#include "geometry_msgs/Pose.h"
 
 
 #include "ros/ros.h"
 #include "sensor_msgs/PointCloud2.h"
-
-
-void GetBallLocationCallback(const sensor_msgs::PointCloud2& msg) {
-    // TODO call cropper/seg
-}
-
 
 int main(int argc, char** argv) {
 
   ros::init(argc, argv, "tennis_ball_finder");
   ros::NodeHandle nh;
 
-//  if (argc < 2) {
-//    ROS_INFO("Usage: rosrun perception point_cloud_demo DATA_DIR");
-//    ros::spinOnce();
-//  }
-
   ros::Publisher crop_pub =
       nh.advertise<sensor_msgs::PointCloud2>("cropped_cloud", 1, true);
   perception::Cropper cropper(crop_pub);
-
-//  ros::Subscriber sub =
-//      nh.subscribe("cloud_in", 1, &perception::Cropper::Callback, &cropper);
-
-
 
   ros::Publisher table_pub =
       nh.advertise<sensor_msgs::PointCloud2>("table_cloud", 1, true);
@@ -56,19 +42,26 @@ int main(int argc, char** argv) {
   ros::Subscriber segment_sub = nh.subscribe(
       "cropped_cloud", 1, &perception::Segmenter::Callback, &segmenter);
 
-    // adding a get_ball_locations_callback to Segmenter just because it's easier to build/add
-    // takes: sensor_msgs::PointCloud2& msg
-  ros::Subscriber segmenter_wrapper = nh.subscribe("get_ball_locations", 1, &perception::Segmenter::GetBallLocationCallback, &segmenter)
-  ros::Subscriber segmenter_wrapper = handle.subscribe("get_ball_locations", 1, GetBallLocationCallback);
+//  ros::Subscriber segmenter_wrapper = handle.subscribe("get_ball_locations", 1, GetBallLocationCallback);
 
+  boost::shared_ptr<const geometry_msgs::Pose> ball_location_message;
+  boost::shared_ptr<const sensor_msgs::PointCloud2> cloud_msg;
   while(true) {
-    boost::shared_ptr<Output message> ball_location_message = ros::topic::waitForMessage<MyOutputMessage>("get_ball_locations", ros::Duration(2));
-    if (ball_location_message != NULL) {
-        ROS_INFO("Got a request for a ball location!");
+  // this should probably just be a subscriver but didnt feel like making all the subsequent subscribers global
+    ball_location_message = ros::topic::waitForMessage<geometry_msgs::Pose>("get_ball_locations", ros::Duration(2));
+    if (ball_location_message != NULL || !ball_location_message) {
+        ROS_INFO("tennis_ball_finder.cpp: Got a request for a ball location!");
         // get next message from input camera (cloud_in) and pass it to Cropper callback
-        boost::shared_ptr<sensor_msgs::PointCloud2> cloud_msg = ros::topic::waitForMessage<MyOutputMessage>("cloud_in", ros::Duration(3));
-        cropper.Callback(*cloud_msg);
-       // perceptor will be listening for fewer messages and automatically pick up the cropped cloud msg
+        cloud_msg = ros::topic::waitForMessage<sensor_msgs::PointCloud2>("cloud_in", ros::Duration(3));
+        if (cloud_msg != NULL) {
+            // perceptor will be listening for fewer messages and automatically pick up the cropped cloud msg
+            cropper.Callback(*cloud_msg);
+        } else {
+            ROS_INFO("Error - didn't get an input msg from camera!");
+        }
+        // set reset pointers just in case (boost pointers dont let you just set them to null)
+        ball_location_message.reset();
+        cloud_msg.reset();
     }
   }
 
